94c37d10a079a3c86a322fbd19f5a1c70ae96b77
Jefferson Santos <jeffrrwpg678@gmail.com>
2025-08-21 15:18:52 -0300

    feat: atualizar ano dinâmico no rodapéate

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>@ 2024 | Jefferson Santos</p>
+      <p>@ {new Date().getFullYear()} | Jefferson Santos</p>
     </footer>
   );
 };
