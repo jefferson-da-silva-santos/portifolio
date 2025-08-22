e49d240e451b37702d4c494d90172b62bef9e3da
Jefferson Santos <jeffrrwpg678@gmail.com>
2025-08-22 16:57:28 -0300

    Refactor skill element styles and update theme configuration
    - Removed the border from the .skill-element class in main.scss.
    - Added new classes for light and dark borders: .skill-element-border-light and .skill-element-border-dark.
    - Updated theme.json to change border property to borderColor for card elements.
    - Enhanced Habilidades component to dynamically apply background colors and border styles based on the current theme.
    - Replaced static text with a state variable for skill description in Habilidades component.

--- src/pages/Habilidades/index.tsx
+++ src/pages/Habilidades/index.tsx
@@ -1,106 +1,123 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useTheme from '../../hooks/useTheme';
 import themeObject from '../../assets/theme.json';
 
 const Habilidades = () => {
   const { theme } = useTheme();
-  
+  const [textSkill, setTextSkill] = useState<string>('*  Passe o mouse sobre as habilidades para saber mais*');
+
+  const styleCardSkill = {
+    backgroundColor: theme === "light" ? themeObject.themes.light.skills.container.background : themeObject.themes.dark.skills.container.background
+  }
+
   return (
-    <div className="groupSkills" id="skills">
+    <div className="groupSkills" id="skills" style={
+      theme === "light" ? {
+        backgroundColor: themeObject.themes.light.skills.container.background
+      } : {
+        backgroundColor: themeObject.themes.dark.skills.container.background
+      }
+    }>
       <section className="skills">
         <article className="groupSkills-primary hidden-scroll-left">
           <div className="dividir-titulo-linha">
             <div className="linhas"></div>
             <div className="linhas"></div>
             <div className="linhas"></div>
-            <h2 className="titleSkills">
+            <h2 className="titleSkills" style={
+              theme === "light" ? {
+                color: themeObject.themes.light.skills.container.colorTitle
+              } : {
+                color: themeObject.themes.dark.skills.container.colorTitle
+              }
+            }>
               &#8249; <span className="letraMonoton">S</span>kills &#8260;
               &#8250;
             </h2>
           </div>
           <p className="textSkills" id="description-skills">
-            * Passe o mouse sobre as habilidades para saber mais * 
+           {textSkill}
           </p>
         </article>
         <article className="groupSkills-secundary hidden-scroll-left">
-          <article className="skill-element nodejs">
+          <article className={`skill-element nodejs ${theme === "light" ? "skill-element-border-light" : "skill-element-border-dark"}`} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_node.webp"
               alt="Logo do Node.js"
             />
           </article>
-          <article className="skill-element express">
+          <article className={"skill-element express"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_express.webp"
               alt="Logo do Express"
             />
           </article>
-          <article className="skill-element nestjs">
+          <article className={"skill-element nestjs"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_nest.webp"
               alt="Logo do NestJS"
             />
           </article>
-          <article className="skill-element react">
+          <article className={"skill-element react"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_react.webp"
               alt="Logo do React"
             />
           </article>
-          <article className="skill-element nextjs">
+          <article className={"skill-element nextjs"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_next.webp"
               alt="Logo do Next.js"
             />
           </article>
-          <article className="skill-element sass">
+          <article className={"skill-element sass"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_sass.webp"
               alt="Logo do Sass"
             />
           </article>
-          <article className="skill-element flutter">
+          <article className={"skill-element flutter"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_flutter.webp"
               alt="Logo do Flutter"
             />
           </article>
-          <article className="skill-element java">
+          <article className={"skill-element java"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_java.webp"
               alt="Logo do Java"
             />
           </article>
-          <article className="skill-element postgresql">
+          <article className={"skill-element postgresql"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_postgres.webp"
               alt="Logo do PostgreSQL"
             />
           </article>
-          <article className="skill-element mysql">
+          <article className={"skill-element mysql"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_mysql.webp"
               alt="Logo do MySQL"
             />
           </article>
-          <article className="skill-element git">
+          <article className={"skill-element git"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_git.webp"
               alt="Logo do Git"
             />
           </article>
-          <article className="skill-element docker">
+          <article className={"skill-element docker"} style={styleCardSkill}>
             <img
               loading="lazy"
               src="https://raw.githubusercontent.com/jefferson-da-silva-santos/imagens-projetos/refs/heads/main/NovoPortifolio/skill_docker.webp"
