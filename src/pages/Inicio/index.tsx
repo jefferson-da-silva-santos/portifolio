a02a89e080ecdf288aabdefd7e818a78bee17511
jefferson-da-silva-santos <jeffrrwpg678@gmail.com>
2026-01-31 06:45:51 -0300

    first

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -6,6 +6,7 @@ import SocialButton from "./Link";
 import { projectImages, socialButtonList } from "../../consts/dataConsts";
 import { handleSetHoverButtonIcon, styleContainer, styleTitle } from "./styles";
 import { stylesButton } from "./styles";
+import eu from '../../assets/image/jeff.png';
 
 const Inicio = () => {
   const { theme, toggleTheme } = useTheme();
@@ -23,8 +24,16 @@ const Inicio = () => {
       className="groupHeader"
       style={styleContainer(theme, themeObject)}
     >
+      <div className="overlay"></div>
       <Navegacao toggleTheme={toggleTheme} />
       <header className="header" id="home">
+        <section data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" className="groupHeader-img">
+          <img
+            loading="lazy"
+            src={eu}
+            alt={projectImages[0].alt}
+          />
+        </section>
         <section data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" className="groupHeader-texts">
           <div className="textsHeader">
             <h1
@@ -33,6 +42,13 @@ const Inicio = () => {
               Jefferson Santos
             </h1>
             <p className="title-secundary-header"></p>
+            <p className="text-header">Desenvolvedor web especializado em criar experiências modernas e eficientes.</p>
+            <div>
+              <div className="bottom-button"></div>
+              <button className="button-header">
+                Veja meu trabalho
+              </button>
+            </div>
           </div>
           <div className="groupHeader-socialMedia">
             {socialButtonList.map((socialButton, index) => (
@@ -50,13 +66,6 @@ const Inicio = () => {
               />
             ))}
           </div>
-        </section>
-        <section data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" className="groupHeader-img">
-          <img
-            loading="lazy"
-            src={projectImages[0].url}
-            alt={projectImages[0].alt}
-          />
         </section>
       </header>
     </div>
